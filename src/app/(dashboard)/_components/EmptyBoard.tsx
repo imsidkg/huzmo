f76a4d8d37abe2a9adfa20b27b08f0b912de17a3
imsidkg <siddharth8271skg2@gmail.com>
2024-08-23 00:28:17 +0530

    commit

--- src/app/(dashboard)/_components/EmptyBoard.tsx
+++ src/app/(dashboard)/_components/EmptyBoard.tsx
@@ -1,15 +1,37 @@
-import React from 'react'
-import Image from "next/image";
-type Props = {}
+"use client";
 
-const EmptyBoard = (props: Props) => {
+import { Button } from "@/components/ui/button";
+import { useOrganization } from "@clerk/nextjs";
+import { useMutation } from "convex/react";
+import Image from "next/image";
+import { title } from "process";
+import { api } from "../../../../convex/_generated/api";
+
+
+const EmptyBoard = () => {
+  const {organization} = useOrganization();
+  const create = useMutation(api.board.create);
+
+  const handleClick = () => {
+    if(!organization) return null;
+
+    create({
+      orgId : organization.id,
+      title : 'Untitled' ,
+    })
+  }
   return (
     <div className="h-full flex flex-col items-center justify-center">
-      <Image src="/empty-favourites.svg" alt="Empty" height={140} width={140} />
+      <Image src="/note.svg" alt="Empty" height={110} width={110} />
       <h2 className="text-2xl font-semibold mt-6">Create your first board</h2>
       <p className="text-muted-foreground text-sm mt-2">
-       Start by creating your board for your organization
+        Start by creating a board for your organization
       </p>
+      <div className="mt-6">
+        <Button size="lg" onClick={handleClick} >
+          Create board
+        </Button>
+      </div>
     </div>
   )
 }
