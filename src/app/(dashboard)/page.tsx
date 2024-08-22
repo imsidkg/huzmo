f86726449efeecfc8cb8aa62d70493789e796fb8
imsidkg <siddharth8271skg2@gmail.com>
2024-08-22 19:32:47 +0530

    commit

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import EmptyOrg from './_components/EmptyOrg'
 
 type Props = {}
 
 const page = (props: Props) => {
   return (
-    <div>page</div>
+    <div className="flex-1 h-[calc(100%-80px)] p-6">
+       <EmptyOrg />
+    </div>
   )
 }
 
