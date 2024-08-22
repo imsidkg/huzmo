6843bab2f0602f67fe90cfee35c93c2d5c88b8bc
imsidkg <siddharth8271skg2@gmail.com>
2024-08-22 18:26:18 +0530

    commit

--- src/app/(dashboard)/_components/sidebar/new-button.tsx
+++ src/app/(dashboard)/_components/sidebar/new-button.tsx
@@ -18,25 +18,25 @@ type Props = {};
 const NewButton = (props: Props) => {
   return (
     <Dialog>
-      <DialogTrigger asChild>
-        <div className="aspect-square ">
-          <Hint
-            label="Create organization"
-            side="right"
-            align="start"
-            sideOffset={18}
-          >
-            <div className="bg-white/25  h-full w-full rounded-md flex items-center justify-center opacity-60 hover:opacity-100 transition  ">
-              <Plus className=" text-white" />
-            </div>
-          </Hint>
-        </div>
-      </DialogTrigger>
-      <DialogContent className=" p-0  border-none max-w-[465px] ">
-        <CreateOrganization routing="hash" />
-      </DialogContent>
-    </Dialog>
-  );
+    <DialogTrigger>
+      <div className="aspect-square">
+        <Hint
+          label="Create organization"
+          side="right"
+          align="start"
+          sideOffset={18}
+        >
+          <div className="bg-white/25 h-full w-full rounded-md flex items-center justify-center opacity-60 hover:opacity-100 transition">
+            <Plus className="text-white" />
+          </div>
+        </Hint>
+      </div>
+    </DialogTrigger>
+    <DialogContent className="p-0  border-none max-w-[465px]">
+      <CreateOrganization  routing="hash"/>
+    </DialogContent>
+  </Dialog>
+  )
 };
 
 export default NewButton;
