5aa86ac0c5fc9da9bd53987f6a7660ea27e5a70e
imsidkg <siddharth8271skg2@gmail.com>
2024-08-23 22:40:47 +0530

    custom hook added

---
+++ src/components/modal/RenameModal.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+} from "@/components/ui/dialog";
+import { useRenameModal } from '../../../store/useRenameModal'
+
+type Props = {}
+
+const RenameModal = (props: Props) => {
+  const {isOpen , onClose} = useRenameModal()
+  return (
+    <Dialog open={isOpen} onOpenChange={onClose}>
+    <DialogContent>
+      <DialogHeader>Edit board title</DialogHeader>
+      <DialogDescription>Enter a new title for this board</DialogDescription>
+      {/* <form onSubmit={handleSubmit} className="space-y-4">
+        <Input
+          disabled={false}
+          required
+          maxLength={60}
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          placeholder="Board title"
+        />
+        <DialogFooter>
+          <DialogClose asChild>
+            <Button type="button" variant="outline">
+              Cancel
+            </Button>
+          </DialogClose>
+          <Button disabled={isLoading} type="submit">
+            Save
+          </Button>
+        </DialogFooter>
+      </form> */}
+    </DialogContent>
+  </Dialog>
+  )
+}
+
+export default RenameModal
