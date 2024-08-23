b9e25fc09917aff7a44238783a572151270d25df
imsidkg <siddharth8271skg2@gmail.com>
2024-08-23 13:05:39 +0530

    commit

--- src/app/(dashboard)/_components/BoardList.tsx
+++ src/app/(dashboard)/_components/BoardList.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import EmptySearch from './EmptySearch';
 import EmptyFavourites from './EmptyFavourites';
 import EmptyBoard from './EmptyBoard';
+import { useQuery } from 'convex/react';
+import { api } from '../../../../convex/_generated/api';
 
 interface BoardListProps {
     orgId: string;
@@ -12,7 +14,7 @@ interface BoardListProps {
   }
 
 const BoardList = ({ orgId, query }: BoardListProps) => {
-    const data= []
+  const data = useQuery(api.boards.get, { orgId, ...query });
     if (!data?.length && query.search) {
         return <EmptySearch/>
       }
@@ -27,7 +29,22 @@ const BoardList = ({ orgId, query }: BoardListProps) => {
     
   return (
   
-    <div>  {JSON.stringify(query)}</div>
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-5 mt-8 pb-10">
+       
+        {data?.map((board) => (
+          <BoardCard
+            key={board._id}
+            id={board._id}
+            title={board.title}
+            imageUrl={board.imageUrl}
+            authorId={board.authorId}
+            authorName={board.authorName}
+            createdAt={board._creationTime}
+            orgId={board.orgId}
+            isFavourite={board.isFavourite}
+          />
+        ))}
+      </div>
   )
 }
 
