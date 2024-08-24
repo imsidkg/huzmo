0322369eb969d3c68f6464ac1ae28b4f61048208
imsidkg <siddharth8271skg2@gmail.com>
2024-08-24 23:57:46 +0530

    commit

--- src/app/boards/[boardId]/_components/Canvas.tsx
+++ src/app/boards/[boardId]/_components/Canvas.tsx
@@ -1,12 +1,15 @@
+'use client'
 import React from 'react'
 import Info from './Info'
 import Toolbar from './Toolbar'
+import { useSelf } from '@liveblocks/react/suspense'
 
 type Props = {
     boardId : string
 }
 
 const Canvas = ({boardId}: Props) => {
+  const myPresence = useSelf((me) => me.presence);
   return (
   <div>
     {boardId}
