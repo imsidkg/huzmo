50130f4f6aa2119f92299de5be6ff1e66d94e3f7
imsidkg <siddharth8271skg2@gmail.com>
2024-08-24 23:12:50 +0530

    commit

--- src/app/api/liveblocks-auth/route.ts
+++ src/app/api/liveblocks-auth/route.ts
@@ -1,25 +1,42 @@
+import { auth, currentUser } from "@clerk/nextjs/server";
 import { Liveblocks } from "@liveblocks/node";
+import { ConvexHttpClient } from "convex/browser";
+import { api } from "../../../../convex/_generated/api";
 
+
+const convex = new ConvexHttpClient(process.env.NEXT_PUBLIC_CONVEX_URL!)
 const liveblocks = new Liveblocks({
-  secret: "pk_dev_eVXJd6Fv5EKE0IHSy0e3_efvNh5KG2ky2I__D2MlohjNhHoTDdR4I80abZlY4z7d",
+  secret: process.env.NEXT_PUBLIC_LIVEBLOCK_SECRET!,
 });
 
 export async function POST(request: Request) {
-  // Get the current user from your database
-  const user = __getUserFromDB__(request);
 
-  // Start an auth session inside your endpoint
-  const session = liveblocks.prepareSession(
-    user.id,
-    { userInfo: user.metadata } // Optional
-  );
+    const authorization = await auth();
+    const user = await currentUser()
+    
+    if(!authorization || !user) {
+        throw new Response ('Unauthorized'  , {status :403})
+    }
 
-  // Use a naming pattern to allow access to rooms with wildcards
-  // Giving the user read access on their org, and write access on their group
-  session.allow(`${user.organization}:*`, session.READ_ACCESS);
-  session.allow(`${user.organization}:${user.group}:*`, session.FULL_ACCESS);
+    const {room}  = await request.json();
+    const board = await convex.query(api.board.get, { id: room });
 
-  // Authorize the user and return the result
-  const { status, body } = await session.authorize();
-  return new Response(body, { status });
+    if (board?.orgId !== authorization.orgId) {
+      return new Response("Unauthorized", { status: 403 });
+    }
+  
+    const userInfo = {
+      name: user.firstName || "Teammate",
+      picture: user.imageUrl,
+    };
+  
+    const session = liveblocks.prepareSession(user.id, { userInfo });
+  
+    if (room) {
+      session.allow(room, session.FULL_ACCESS);
+    }
+  
+    const { status, body } = await session.authorize();
+  
+    return new Response(body, { status });
 }
--- src/components/Room.tsx
+++ src/components/Room.tsx
@@ -11,7 +11,7 @@ type Props = {}
 const Room = ({children , roomId} : {children : ReactNode , roomId:string}) => {
     
   return (
-    <LiveblocksProvider publicApiKey={"pk_dev_eVXJd6Fv5EKE0IHSy0e3_efvNh5KG2ky2I__D2MlohjNhHoTDdR4I80abZlY4z7d"}>
+    <LiveblocksProvider publicApiKey={process.env.NEXT_PUBLIC_LIVEBLOCK_SECRET!}>
     <RoomProvider id={roomId} initialPresence={{}}>
         <ClientSideSuspense fallback = {<div>Loading ....</div>}>
             {() => children}
