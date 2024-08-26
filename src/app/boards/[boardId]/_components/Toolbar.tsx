75ae2742d5eb3fd6445426c1d7991e72349a04bf
imsidkg <siddharth8271skg2@gmail.com>
2024-08-27 01:20:25 +0530

    commit

--- src/app/boards/[boardId]/_components/Toolbar.tsx
+++ src/app/boards/[boardId]/_components/Toolbar.tsx
@@ -1,10 +1,88 @@
-import React from 'react'
+import { CanvasMode, CanvasState, LayerType } from "@/types/canvas";
+import {
+  Circle,
+  MousePointer2,
+  Pencil,
+  Redo2,
+  Square,
+  StickyNote,
+  Type,
+  Undo2,
+} from "lucide-react";
+import ToolButton from "./ToolButton";
+
+
 
 type Props = {}
 
 const Toolbar = (props: Props) => {
   return (
-    <div>Toolbar</div>
+    <div className="absolute top-[50%] -translate-y-[50%] left-2 flex flex-col gap-y-4">
+    <div className="bg-white rounded-md p-1.5 flex gap-1 flex-col items-center shadow-md">
+      <ToolButton
+        label="Select"
+        icon={MousePointer2}
+        onClick={}
+        isActive={
+          
+        }
+      />
+      <ToolButton
+        label="Text"
+        icon={Type}
+        onClick={}
+        
+        isActive={
+         
+        }
+      />
+      <ToolButton
+        label="Sticky note"
+        icon={StickyNote}
+        onClick={}
+        isActive={
+          
+        }
+      />
+      <ToolButton
+        label="Rectangle"
+        icon={Square}
+        onClick={}
+        isActive={
+       
+        }
+      />
+      <ToolButton
+        label="Ellipse"
+        icon={Circle}
+        onClick={}
+        
+        isActive={
+         
+        }
+      />
+      <ToolButton
+        label="Pen"
+        icon={Pencil}
+        onClick={}
+        isActive={}
+      />
+    </div>
+    <div className="bg-white rounded-md p-1.5 flex flex-col items-center shadow-md">
+      <ToolButton
+        label="Undo"
+        icon={Undo2}
+        onClick={}
+        disabled={}
+      />
+      <ToolButton
+        label="Redo"
+        icon={Redo2}
+        onClick={}
+        disabled={}
+      />
+    </div>
+  </div>
   )
 }
 
