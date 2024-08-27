fab820b9787151bea75c2c9c4d6fd7779612b6e9
imsidkg <siddharth8271skg2@gmail.com>
2024-08-27 17:41:13 +0530

    commit

--- src/app/boards/[boardId]/_components/Cursor.tsx
+++ src/app/boards/[boardId]/_components/Cursor.tsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import { useOther } from '@liveblocks/react/suspense'
+import React, { memo } from 'react'
 
-type Props = {}
+type Props = {
+    connectionId : number
+}
 
-const Cursor = (props: Props) => {
+const Cursor = memo(({connectionId}: Props) => {
+    const info = useOther(connectionId, (user) => user?.info);
+    const cursor =  useOther(connectionId , (user) => user.presence.cursor);
+
+    const name = info?.name || 'Teammate';
+    if(!cursor ) return null;
+     
+    const {x , y } = cursor
+    
+
   return (
     <div>Cursor</div>
   )
-}
+})
 
 export default Cursor
--- src/app/boards/[boardId]/_components/CursorPresence.tsx
+++ src/app/boards/[boardId]/_components/CursorPresence.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import Cursor from './Cursor'
+import { useOthersConnectionIds } from '@liveblocks/react/suspense'
 
 type Props = {}
 
 const CursorPresence = (props: Props) => {
+    const ids = useOthersConnectionIds()
   return (
-    <div>CursorPresence</div>
+    <div>
+       {ids.map((connectionId) => {
+        return(
+            <Cursor connectionId={connectionId}/>
+        )
+       })}
+    </div>
   )
 }
 
