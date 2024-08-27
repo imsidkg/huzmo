53d937af0fd64d8047c2000026b3a2fc68950b2b
imsidkg <siddharth8271skg2@gmail.com>
2024-08-27 19:39:32 +0530

    commit

--- src/components/Room.tsx
+++ src/components/Room.tsx
@@ -3,19 +3,27 @@ import React, { ReactNode } from 'react'
 import {
     LiveblocksProvider,
     RoomProvider,
-    ClientSideSuspense,
+    ClientSideSuspense, 
+  
   } from "@liveblocks/react/suspense";
+import { LiveList, LiveMap, LiveObject } from '@liveblocks/client';
+import { Layer , Color } from '../../types/canvas';
 
 type Props = {}
 
 const Room = ({children , roomId} : {children : ReactNode , roomId:string}) => {
-    
+  
   return (
     //@ts-ignore
     <LiveblocksProvider 
     throttle={16}
-    authEndpoint="/api/liveblocks-auth">
-    <RoomProvider id={roomId} initialPresence={{}}>
+    authEndpoint="/api/liveblocks-auth"
+    
+    >
+    <RoomProvider id={roomId} initialPresence={{}}   initialStorage={{
+        layers: new LiveMap<string, LiveObject<Layer>>(),
+        layerIds: new LiveList<string>(["user1", "user2"]),
+      }}>
         <ClientSideSuspense fallback = {<div>Loading ....</div>}>
             {() => children}
         </ClientSideSuspense>
