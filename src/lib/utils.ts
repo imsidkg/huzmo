c864fa8e6da32be676f6aab6e4c722cf76796a58
imsidkg <siddharth8271skg2@gmail.com>
2024-08-29 02:13:05 +0530

    commit

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,6 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
-import { Camera } from "../../types/canvas";
+import { Camera, Color } from "../../types/canvas";
 const COLORS = ["#DC2626", "#D97706", "#059669", "#7C3AED", "#DB2777"];
 
 export const connectionIdToColor = (connectionId: number) => {
@@ -15,4 +15,10 @@ export function pointerEventToCanvasPoint (e : React.PointerEvent , camera : Cam
   const x  = e.clientX - camera.x ;
   const y = e.clientY - camera.y;
   return {x,y}
+}
+
+export function colorToCss(color: Color) {
+  return `#${color.r.toString(16).padStart(2, "0")}${color.g
+    .toString(16)
+    .padStart(2, "0")}${color.b.toString(16).padStart(2, "0")}`;
 }
