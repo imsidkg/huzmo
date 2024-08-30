3fb139bc7d862c3e5ec75ed8f1eb02a9f88b12e8
imsidkg <siddharth8271skg2@gmail.com>
2024-08-30 12:53:28 +0530

    commit

--- src/app/boards/[boardId]/_components/CursorPresence.tsx
+++ src/app/boards/[boardId]/_components/CursorPresence.tsx
@@ -1,20 +1,54 @@
-import React from 'react'
-import Cursor from './Cursor'
-import { useOthersConnectionIds } from '@liveblocks/react/suspense'
+"use client";
 
-type Props = {}
 
-const CursorPresence = (props: Props) => {
-    const ids = useOthersConnectionIds()
+const Cursors = () => {
+  const ids = useOthersConnectionIds();
+
   return (
-    <div>
-       {ids.map((connectionId) => {
-        return(
-            <Cursor connectionId={connectionId}/>
-        )
-       })}
-    </div>
-  )
-}
+    <>
+      {ids.map((id) => (
+        <Cursor key={id} connectionId={id} />
+      ))}
+    </>
+  );
+};
 
-export default CursorPresence
+const Draft = () => {
+  const others = useOthersMapped(
+    (other) => ({
+      pencilDraft: other.presence.pencilDraft,
+      pencilColor: other.presence.pencilColor,
+    }),
+    shallow
+  );
+
+  return (
+    <>
+      {others.map(([key, other]) => {
+        if (other.pencilDraft) {
+          return (
+            <Path
+              key={key}
+              x={0}
+              y={0}
+              points={other.pencilDraft}
+              fill={other.pencilColor ? colorToCss(other.pencilColor) : "#000"}
+            />
+          );
+        }
+        return null;
+      })}
+    </>
+  );
+};
+
+export const CursorPresence = memo(() => {
+  return (
+    <>
+      <Draft />
+      <Cursors />
+    </>
+  );
+});
+
+CursorPresence.displayName = "CursorPresence";
